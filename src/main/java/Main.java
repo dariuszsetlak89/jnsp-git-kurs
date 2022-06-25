c075537b9fce6395bf3b47adc0ec7f9d809c0425
KryptoProgramista <kryptoprogramista89@gmail.com>
2022-06-25 21:20:34 +0200

    Zmiana Hello World

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.printf("Hello Poland");
     }
 
     private void displayText() {
