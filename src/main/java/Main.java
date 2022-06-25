f52389903ce8db5a7cd7e69e87b56fedd43320ba
KryptoProgramista <kryptoprogramista89@gmail.com>
2022-06-25 21:23:22 +0200

    Zmiana w drugim repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello World");
     }
+
+    private void displayText() {
+        System.out.printf("Zmiana w drugim repo");
+    }
 }
